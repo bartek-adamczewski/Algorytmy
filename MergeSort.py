1e542b31909935c0236e21ff4ca10ffae33a2da8
bartek-adamczewski <bartosz.adamczewski1@wp.pl>
2022-03-12 16:16:34 +0100

    :)

--- MergeSort.py
+++ MergeSort.py
@@ -1,7 +1,29 @@
 def merge_sort(lst):
-    left, right = lst[:len(lst)//2], lst[len(lst)//2:]     # Dzielenie głównej listy na dwie
+    if len(lst) > 1:
+        left, right = lst[:len(lst)//2], lst[len(lst)//2:]     # Dzielenie głównej listy na dwie
 
-    merge_sort(right)
-    merge_sort(left)
+        merge_sort(left)                                       # Rekurencja
+        merge_sort(right)
 
-    l_ind, r_ind, main_ind = 0, 0, 0
+        l_ind = 0
+        r_ind = 0
+        main_ind = 0
+
+        while l_ind < len(left) and r_ind < len(right):        # dodawanie mniejszej wartości do głównej listy
+            if left[l_ind] < right[r_ind]:
+                lst[main_ind] = left[l_ind]
+                l_ind += 1
+            else:
+                lst[main_ind] = right[r_ind]
+                r_ind += 1
+            main_ind += 1
+
+        while l_ind < len(left):                               # gdy dodano wszystkie elementy z jednej z dwóch list
+            lst[main_ind] = left[l_ind]
+            l_ind += 1
+            main_ind += 1
+
+        while r_ind < len(right):
+            lst[main_ind] = right[r_ind]
+            r_ind += 1
+            main_ind += 1
