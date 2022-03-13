6c88b1b2700d1f15ccb0e70593d1323cf8896ee0
bartek-adamczewski <bartosz.adamczewski1@wp.pl>
2022-03-13 19:58:46 +0100

    :)

--- CountSort.py
+++ CountSort.py
@@ -1,19 +1,20 @@
 def count_sort(lst):
-    values = [0 for i in range(max(lst) + 1)]
+    if lst:
+        values = [0 for i in range(max(lst) + 1)]
 
-    for number in lst:
-        values[number] += 1
+        for number in lst:
+            values[number] += 1
 
-    for index in range(1, len(values)):
-        values[index] = values[index - 1] + values[index]
+        for index in range(1, len(values)):
+            values[index] = values[index - 1] + values[index]
 
-    answer = [0 for loop in range(len(lst))]
-    for value in lst:
-        index = values[value] - 1
-        answer[index] = value
-        values[value] -= 1
+        answer = [0 for loop in range(len(lst))]
+        for value in lst:
+            index = values[value] - 1
+            answer[index] = value
+            values[value] -= 1
 
-    return answer
+        return answer
 
 
 
