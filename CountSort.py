fb062e80af9329fea34107ec5c63106e49a79d87
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2022-03-13 20:07:16 +0100

    Teraz naprawdę naprawione

--- CountSort.py
+++ CountSort.py
@@ -1,20 +1,21 @@
 def count_sort(lst):
-    if lst:
-        values = [0 for i in range(max(lst) + 1)]
+    if len(lst) == 1 or len(lst) == 0:
+        return lst
 
-        for number in lst:
-            values[number] += 1
+    values = [0 for i in range(max(lst) + 1)]
+    for number in lst:
+        values[number] += 1
 
-        for index in range(1, len(values)):
-            values[index] = values[index - 1] + values[index]
+    for index in range(1, len(values)):
+        values[index] = values[index - 1] + values[index]
 
-        answer = [0 for loop in range(len(lst))]
-        for value in lst:
-            index = values[value] - 1
-            answer[index] = value
-            values[value] -= 1
+    answer = [0 for loop in range(len(lst))]
+    for value in lst:
+        index = values[value] - 1
+        answer[index] = value
+        values[value] -= 1
 
-        return answer
+    return answer
 
 
 
