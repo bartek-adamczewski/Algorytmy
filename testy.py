4cb7d3fab073110f6e22998738cdd050f5e8db3d
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2022-03-13 19:55:03 +0100

    Created benchmark

--- testy.py
+++ testy.py
@@ -12,8 +12,8 @@ from QuickSortMiddle import sort as quick_sort_middle
 from SelectionSort import selection_sort
 
 
-def get_test_data() -> List[int]:
-    data = list(range(1, 100))
+def get_test_data(n: int) -> List[int]:
+    data = list(range(1, n))
     random.shuffle(data)
     return data
 
@@ -25,7 +25,7 @@ def get_sorted(data: List[int]) -> List[int]:
 
 
 class TestRandomData(TestCase):
-    data = get_test_data()
+    data = get_test_data(100)
     sorted = get_sorted(data)
 
     def test_bubble_sort(self):
@@ -61,3 +61,78 @@ class TestRandomData(TestCase):
         copy = self.data.copy()
         heap_sort(copy, len(copy))
         self.assertEqual(self.sorted, copy)
+
+class TestSingleElement(TestCase):
+    data = get_test_data(1)
+    sorted = get_sorted(data)
+
+    def test_bubble_sort(self):
+        res = bubble_sort(self.data.copy())
+        self.assertEqual(self.sorted, res)
+
+    def test_insertion_sort(self):
+        res = insertion_sort(self.data.copy())
+        self.assertEqual(self.sorted, res)
+
+    def test_selection_sort(self):
+        res = selection_sort(self.data.copy())
+        self.assertEqual(self.sorted, res)
+
+    def test_count_sort(self):
+        res = count_sort(self.data.copy())
+        self.assertEqual(self.sorted, res)
+
+    def test_merge_sort(self):
+        copy = self.data.copy()
+        merge_sort(copy)
+        self.assertEqual(self.sorted, copy)
+
+    def test_quick_sort_last(self):
+        res = quick_sort(self.data.copy())
+        self.assertEqual(self.sorted, res)
+
+    def test_quick_sort_middle(self):
+        res = quick_sort_middle(self.data.copy())
+        self.assertEqual(self.sorted, res)
+
+    def test_heap_sort(self):
+        copy = self.data.copy()
+        heap_sort(copy, len(copy))
+        self.assertEqual(self.sorted, copy)
+
+class TestEmptyList(TestCase):
+    data = []
+    sorted = []
+
+    def test_bubble_sort(self):
+        res = bubble_sort(self.data.copy())
+        self.assertEqual(self.sorted, res)
+
+    def test_insertion_sort(self):
+        res = insertion_sort(self.data.copy())
+        self.assertEqual(self.sorted, res)
+
+    def test_selection_sort(self):
+        res = selection_sort(self.data.copy())
+        self.assertEqual(self.sorted, res)
+
+    def test_count_sort(self):
+        res = count_sort(self.data.copy())
+        self.assertEqual(self.sorted, res)
+
+    def test_merge_sort(self):
+        copy = self.data.copy()
+        merge_sort(copy)
+        self.assertEqual(self.sorted, copy)
+
+    def test_quick_sort_last(self):
+        res = quick_sort(self.data.copy())
+        self.assertEqual(self.sorted, res)
+
+    def test_quick_sort_middle(self):
+        res = quick_sort_middle(self.data.copy())
+        self.assertEqual(self.sorted, res)
+
+    def test_heap_sort(self):
+        copy = self.data.copy()
+        heap_sort(copy, len(copy))
